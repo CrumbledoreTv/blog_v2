bc346a67f575872b515173eaa41cca01c91255d6
Your Name <you@example.com>
2017-06-02 14:04:20 +0200

    pages admin

---
+++ pages/admin.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Blog</title>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
+     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="../css/style.css" media="screen" title="no title">
+</head>
+
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Blog</a></li>
+
+          <!-- <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Visiteur<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Deconnexion</a></li>
+            </ul>
+          </li> -->
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administrateur <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a><img class="img-responsive" src="../img/placeholder.jpg" alt="" /></a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Parametres</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Deconnexion</a></li>
+            </ul>
+          </li>
+              <!-- <li><a href="#">Se connecter</a></li>
+              <li><a href="#">S'enregistrer</a></li> -->
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+
+  <!-- ************************************************************************************
+  ************************************************************************************
+  ************************************************************************************ -->
+
+  <div class="container">
+    <div class="row">
+
+  <div class="alert alert-success">
+    <h1>Mes articles en ligne </h1>
+    <button type="button" name="button"><i class="fa fa-plus-circle" aria-hidden="true"></i> Creer un article</button>
+    <button type="button" name="button"><i class="fa fa-file-text-o" aria-hidden="true"></i> Mes brouillons</button>
+  </div>
+
+  <div class="well col-md-12">
+    <button type="button" name="button"><i class="fa fa-trash" aria-hidden="true"></i> Supprimer</button>
+    <button type="button" name="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Modifier</button>
+    <button type="button" name="button"><i class="fa fa-file" aria-hidden="true"></i> Ajouter aux brouillons</button>
+
+    <h2 class="col-md-12">Bienvenue sur mon blog !</h2>
+    <img class="col-md-6 col-md-offset-3 img-responsive"src="../img/placeholder.png" alt="" />
+    <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+      et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e
+    x ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+     nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+
+    <div class="well col-md-12">
+      <button type="button" name="button"><i class="fa fa-trash" aria-hidden="true"></i> Supprimer</button>
+      <button type="button" name="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Modifier</button>
+      <button type="button" name="button"><i class="fa fa-file" aria-hidden="true"></i> Ajouter aux brouillons</button>
+      <h2 class="col-md-12">Bienvenue sur mon blog !</h2>
+      <img class="col-md-6 col-md-offset-3 img-responsive" src="../img/placeholder.png" alt="" />
+      <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e
+      x ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+       nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+
+  </div><!--Row -->
+  </div><!--container-->
+
+  <footer> copiryght </footer>
+
+
+
+</body>
+
+<script src="https://use.fontawesome.com/5ae75527aa.js"></script>
+<script type="text/javascript" src="../js/index.js"></script>
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+
+</html>
