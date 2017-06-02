d0bb4fcb1abeeeba9978499ba5309179d69911ee
Your Name <you@example.com>
2017-06-02 14:03:41 +0200

    Page daccueil

---
+++ _index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Blog</title>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
+     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title">
+</head>
+
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Blog</a></li>
+
+          <!-- <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Visiteur<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Deconnexion</a></li>
+            </ul>
+          </li> -->
+          <!-- <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administrateur <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a><img src="{{ asset('assets/img/placeholder.jpg') }}" alt="" /></a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Parametres</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Deconnexion</a></li>
+            </ul>
+          </li> -->
+              <li><a href="#">Se connecter</a></li>
+              <li><a href="#">S'enregistrer</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+
+<!-- ************************************************************************************
+************************************************************************************
+************************************************************************************ -->
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-7">
+      <div class="well col-md-12">
+          <h2> Bienvenue sur mon blog !</h2>
+          <img class="img-responsive col-md-6 col-md-offset-3"src="img/placeholder.png" alt="" />
+          <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e
+          x ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+           nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+        <div class="well col-md-12">
+            <h2> Bienvenue sur mon blog !</h2>
+            <img class="img-responsive col-md-6 col-md-offset-3"src="img/placeholder.png" alt="" />
+            <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e
+            x ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+             nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+
+          <div class="well col-md-12">
+              <h2> Bienvenue sur mon blog !</h2>
+              <img class="img-responsive col-md-6 col-md-offset-3"src="img/placeholder.png" alt="" />
+              <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e
+              x ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+    </div>
+
+    <div class="col-md-4">
+
+      <div class="panel panel-default">
+        <div class="panel-heading">A propos</div>
+        <div class="panel-body">
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa
+          qui officia deserunt mollit anim id est laborum.
+
+          </p>
+        </div>
+      </div>
+
+      <div class="panel panel-default">
+        <div class="panel-heading">Les derniers articles</div>
+        <div class="panel-body"><a href="#">Article 1</a></div>
+        <div class="panel-body"><a href="#">Article 2</a></div>
+        <div class="panel-body"><a href="#">Article 3</a></div>
+      </div>
+
+
+
+
+
+
+  </div>
+
+
+    <!--  <div class="well col-md-6">
+     <div class="col-md-12 panel panel-default">
+       <div class="panel-heading">
+
+       </div>
+       <div class="panel-body">
+
+       </div>
+
+     </div>
+
+      </div>
+
+    <div class="well col-md-6">
+      <h2> Bienvenue sur mon blog !</h2>
+      <img class="img-responsive col-md-6 col-md-offset-3"src="img/placeholder.png" alt="" />
+      <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e
+      x ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+       nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div> -->
+
+</div><!--row-->
+  </div><!--container-->
+
+  <footer> copiryght </footer>
+
+
+
+</body>
+
+<script src="https://use.fontawesome.com/5ae75527aa.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+</html>
